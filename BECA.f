848dec30754db0d2d768ca00a088a7cb6d7ef9e0
GerardoManz <92065706+GerardoManz@users.noreply.github.com>
2022-09-28 22:39:54 -0500

    Update BECA.f

--- BECA.f
+++ BECA.f
@@ -1,41 +1,43 @@
-        program BECA
+     program becas
         implicit none
-
-        integer edad;
-        Real prom, beca;
-        character (1) tipo
-
-        beca = 0
-        write(*,*) 'Cuantos a¤os tienes'
+        integer edad
+        real prom
+        
+        write (*,*) "Introduce la edad del alumno: "
         read(*,*)edad
-        write(*,*) 'Cual es tu promedio'
+        
+        write (*,*) "Introduce el promedio del alumno: "
         read(*,*)prom
-
-        if (edad > 18) then
-        if(prom >= 9) then
-        beca = 2000
-        else if(prom>=7.5) then
-        beca = 1000
-        else if(prom >= 6) then
-        beca = 500
-        else
-        write(*,*) 'Se te enviara una carta incitandote a mejorar y'
-        end if
         
-        else
-        if(prom >= 9) then
-        beca = 3000
-        else if(prom>=8) then
-        beca = 2000
-        else if(prom >= 6) then
-        beca = 100
-        else
-        write(*,*) 'Se te enviara una carta incitandote a mejorar y'
+        if (edad>18)then
+           if (prom>=9)then
+           write (*,*) "Beca de 2000 "
+           end if
+                if ((prom>=7.5).and.(prom<9))then
+                write (*,*) "Beca de 1000 "
+                end if
+                if ((prom>=6).and.(prom<7.5)) then
+                write (*,*) "Beca de 500 "
+                end if
+                if(prom<6)then
+                write (*,*) "Estudia mas en el proximo ciclo escolar. "
+                end if
         end if
+                if (edad<=18)then
+           if (prom>=9)then
+           write (*,*) "Beca de 3000 "
+           end if
+                if ((prom>=8).and.(prom<9))then
+                write (*,*) "Beca de 2000 "
+                end if
+                if ((prom>=6).and.(prom<8)) then
+                write (*,*) "Beca de 100 "
+                end if
+                if(prom<6)then
+                write (*,*) "Estudia mas en el proximo ciclo escolar. "
+                end if
         end if
 
-        
-        write(*,*) 'De beca te llegara: '
-        write(*,*) beca
+           
         pause
-        end program BECA
+        end program becas
